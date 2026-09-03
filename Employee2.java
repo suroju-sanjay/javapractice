d6b06b1bccd2e32956b74468be987d82baaf3d64
suroju-sanjay <sanjaysuroju@gmail.com>
2026-09-03 18:32:22 +0530

    updated Employee2

--- Employee2.java
+++ Employee2.java
@@ -7,18 +7,13 @@ int salary;
  Employee2(int id,String name,int salary) {
 	 this.id=id;
 	 this.name=name;
-	 this.salary=salary;
-	 
+	 this.salary=salary;	 
 }
- 
 	public static void main(String[] args) {
 		Employee2 e1=new Employee2(101,"tharun",70000);
 		e1.display();
 	Employee2 e2= new Employee2(101,"prashanth",50000);
 	e2.display();
-	
-		
-
 	}
 void display() {
 	System.out.println("employee id :"+id);
